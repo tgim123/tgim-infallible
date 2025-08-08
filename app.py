aa691ef7d594cc82c41c2dadf24706fd9db5dbea
tgim123 <rinoldyumiekh8@gmail.com>
2025-08-08 03:03:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,3 @@ def root():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
